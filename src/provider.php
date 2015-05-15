509d292b4ef43e0d11ceae3b25e0db68dc03d902
Daniel Gehn <inad.gamer@gmail.com>
2015-05-16 00:29:58 +0200

    Changed fileversion

--- src/provider.php
+++ src/provider.php
@@ -3,7 +3,7 @@
  * Basic Synology Hosting Provider Boilerplate
  *
  * @author Daniel Gehn <me@theinad.com>
- * @version 0.1a
+ * @version 0.1b
  * @copyright 2015 Daniel Gehn
  * @license http://opensource.org/licenses/MIT Licensed under MIT License
  */
